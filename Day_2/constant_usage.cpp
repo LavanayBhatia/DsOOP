22906ddb8904435eed1ac81c226b96b8daa1f72e
Lavanay Bhatia <vvenm333@gmail.com>
2025-07-17 11:00:38 +0530

    Create constant_usage.cpp

---
+++ Day_2/constant_usage.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define PI 3.14
+using namespace std;
+
+int main() {
+    const int x = 10;
+    cout << "Const value: " << x << endl;
+    cout << "Macro value: " << PI;
+    return 0;
+}
